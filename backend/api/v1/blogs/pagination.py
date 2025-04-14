23213422d121b2e450c007240b4015063fbe2d9d
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-14 17:33:25 +0300

    Added custom pagination class for blog

---
+++ backend/api/v1/blogs/pagination.py
@@ -0,0 +1,7 @@
+from rest_framework.pagination import LimitOffsetPagination
+
+
+class BlogPagination(LimitOffsetPagination):
+    """Настройка пагинации для списка блогов. """
+    default_limit = 20
+    max_limit = 100
