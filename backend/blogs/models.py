f060cb12601025927d1e4dc30f86ae4ab124ce33
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-14 19:38:47 +0300

    added created_at field

--- backend/blogs/models.py
+++ backend/blogs/models.py
@@ -10,6 +10,7 @@ class Blog(models.Model):
     slug = models.SlugField(unique=True, max_length=100, help_text="Имя блога для url")
     title = models.CharField(unique=True, max_length=255, help_text="Заголовок блога")
     description = models.CharField(max_length=500, null=True, blank=True, help_text="Описание блога")
+    created_at = models.DateTimeField(auto_now_add=True)
 
     authors = models.ManyToManyField(User, related_name="blogs", help_text="Авторы блога", through="BlogAuthor")
 
