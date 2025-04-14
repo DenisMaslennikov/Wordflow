47df6e6f5679eea0744f1620ace5ad345e6f09fa
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-14 12:14:57 +0300

    Added blog model

--- backend/blogs/models.py
+++ backend/blogs/models.py
@@ -1,2 +1,11 @@
+from django.db import models
 
 # Create your models here.
+
+
+class Blogs(models.Model):
+    """Модель блога."""
+
+    slug = models.SlugField(unique=True, max_length=100, help_text="Имя блога для url")
+    title = models.CharField(unique=True, max_length=255, help_text="Заголовок блога")
+    description = models.CharField(max_length=500, null=True, blank=True, help_text="Описание блога")
