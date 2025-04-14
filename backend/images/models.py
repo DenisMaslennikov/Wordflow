5ef0791d403a205225c0baabb57f91a92aab89e8
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-14 14:11:05 +0300

    Added Image model

--- backend/images/models.py
+++ backend/images/models.py
@@ -1,2 +1,20 @@
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.db import models
 
-# Create your models here.
+from blogs.models import Blog
+
+User = get_user_model()
+
+
+class Image(models.Model):
+    """Модель для хранения изображений."""
+
+    user = models.ForeignKey(
+        User, on_delete=models.SET_DEFAULT, default=2, help_text="Идентификатор пользователя загрузившего изображение"
+    )
+    blog = models.ForeignKey(
+        Blog, on_delete=models.CASCADE, help_text="Идентификатор блога в который было загружено изображение"
+    )
+    alt = models.CharField(max_length="100", null=True, blank=True, help_text="Описание изображения")
+    image = models.ImageField(upload_to=settings.MEDIA_ROOT, help_text="Изображение")
