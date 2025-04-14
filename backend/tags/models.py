225cbfd1c1900bbc28dcfc7c761d1392fd1746a4
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-14 13:09:26 +0300

    Added tag model

--- backend/tags/models.py
+++ backend/tags/models.py
@@ -1,2 +1,11 @@
+from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    """Модель метки для постов."""
+
+    name = models.CharField(max_length=30, help_text="Название тега")
+    slug = models.SlugField(max_length=40, help_text="Слаг")
+
+    def __str__(self):
+        return self.name
