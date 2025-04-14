8426c5d8b515d1e27b8dcdf30acb7ecbf3df347e
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-14 19:39:22 +0300

    added fields to serializers

--- backend/api/v1/blogs/serializers.py
+++ backend/api/v1/blogs/serializers.py
@@ -14,26 +14,31 @@ class BlogDetailedSerializer(serializers.ModelSerializer):
         """Метакласс сериализатора."""
 
         model = Blog
-        fields = ("id", "slug", "title", "description", "authors")
+        fields = ("id", "slug", "title", "description", "authors", "created_at")
 
 
 class BlogCreateUpdateSerializer(serializers.ModelSerializer):
     """Сериализатор для операций записи модели Blog."""
 
+    id = serializers.IntegerField(read_only=True)
+    authors = UsernameUserSerializer(many=True, read_only=True)
+    created_at = serializers.DateTimeField(read_only=True)
+
     class Meta:
         """Метакласс сериализатора."""
 
         model = Blog
-        fields = ("slug", "title", "description")
+        fields = ("slug", "title", "description", "created_at", "authors", "id")
 
 
 class BlogListSerializer(serializers.ModelSerializer):
     """Сериализатор для списков модели Blog."""
 
     id = serializers.IntegerField(read_only=True)
+    created_at = serializers.DateTimeField(read_only=True)
 
     class Meta:
         """Метакласс сериализатора."""
 
         model = Blog
-        fields = ("id", "title", "description",)
+        fields = ("id", "title", "description", "created_at")
