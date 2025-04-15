a77b93f3083628d91ce8f0210585d6d2bdc09b7b
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-15 11:40:51 +0300

    Added query string search parameter for swagger

--- backend/api/v1/main_page/views.py
+++ backend/api/v1/main_page/views.py
@@ -1,4 +1,5 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework import viewsets, mixins, filters
 from rest_framework.generics import ListAPIView
 
@@ -11,11 +12,24 @@ from posts.models import Post
 class MainPagePostsAPIView(ListAPIView):
     """Апи вью для постов на главной странице."""
 
-    queryset = Post.objects.all().select_related("user")
+    queryset = Post.objects.all().select_related("user").prefetch_related("tags")
     serializer_class = PostListSerializer
     filter_backends = (PrioritizedPostSearchFilter, filters.OrderingFilter, DjangoFilterBackend)
     search_fields = ("title", "content")
-    filterset_fields = ("user__username",)
+    filterset_fields = ("user__id", "tags__id")
     ordering_fields = ("title", "content", "published_at", "user__username")
     ordering = ("-published_at",)
     pagination_class = PostPagination
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="search",
+                type=str,
+                location=OpenApiParameter.QUERY,
+                description="Search by title and content",
+            ),
+        ]
+    )
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
