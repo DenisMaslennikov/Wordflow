882e00f0e95f5676b31fb9eb1cc943765b4ca442
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-15 16:16:52 +0300

    Fix validator error

--- backend/api/v1/tags/serializers.py
+++ backend/api/v1/tags/serializers.py
@@ -18,3 +18,4 @@ class TagSerializer(serializers.ModelSerializer):
         """Валидация уникальности тега"""
         if Tag.objects.filter(name=name).exists():
             raise serializers.ValidationError({"name": "Тег с таким именем уже есть"})
+        return name
