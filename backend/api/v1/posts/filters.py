37b4f3f93fd54ce0ce796aca703c97f6a23917d6
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-15 11:12:54 +0300

    Added PrioritizedPostSearchFilter

---
+++ backend/api/v1/posts/filters.py
@@ -0,0 +1,30 @@
+from rest_framework import filters
+from django.db.models import Case, When, Q, IntegerField, Value
+
+
+class PrioritizedPostSearchFilter(filters.BaseFilterBackend):
+    """Фильтр поиска с приоритетом."""
+
+    def filter_queryset(self, request, queryset, view):
+        """Фильтрация кверисета."""
+        search_query = request.query_params.get("search", "")
+
+        term = search_query.strip()
+
+        if not term:
+            return queryset
+
+        prioritized_filtered_qs = (
+            queryset.annotate(
+                priority=Case(
+                    When(title__icontains=term, then=Value(1)),
+                    When(content__icontains=term, then=Value(2)),
+                    default=Value(3),
+                    output_field=IntegerField(),
+                )
+            )
+            .filter(Q(title__icontains=term) | Q(content__icontains=term))
+            .order_by("priority")
+        )
+
+        return prioritized_filtered_qs
