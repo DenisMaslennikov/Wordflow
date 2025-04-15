9d3bd4dfdebbacaa488a5b85691178bf6582c8a0
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-15 10:08:07 +0300

    added main page ViewSet

---
+++ backend/api/v1/main_page/views.py
@@ -0,0 +1,19 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets, mixins, filters
+
+from api.v1.posts.pagination import PostPagination
+from api.v1.posts.serializers import PostListSerializer
+from posts.models import Post
+
+
+class MainPagePostsViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+    """Вью сет для постов на главной странице."""
+
+    queryset = Post.objects.all().select_related("user")
+    serializer_class = PostListSerializer
+    filter_backends = (filters.SearchFilter, filters.OrderingFilter, DjangoFilterBackend)
+    search_fields = ("title", "content")
+    filterset_fields = ("user__username",)
+    ordering_fields = ("title", "content", "published_at", "user__username")
+    ordering = ("-published_at",)
+    pagination_class = PostPagination
