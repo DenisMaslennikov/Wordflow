0bd7c4b4be7efaf1c135313dc63cbbb344087682
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-15 11:26:19 +0300

    Added MainPageAPIView to routes

--- backend/api/v1/urls.py
+++ backend/api/v1/urls.py
@@ -2,6 +2,7 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from api.v1.blogs.views import BlogViewSet
+from api.v1.main_page.views import MainPagePostsAPIView
 from api.v1.users.views import RegisterView
 
 router = DefaultRouter()
@@ -12,4 +13,5 @@ app_name = "v1"
 urlpatterns = [
     path("", include(router.urls)),
     path("user/register/", RegisterView.as_view(), name="register"),
+    path("main_page/posts/", MainPagePostsAPIView.as_view(), name="main_page_posts"),
 ]
