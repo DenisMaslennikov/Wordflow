dea8d8585f8e873c300066b881ccfdf4febae1db
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-15 12:21:51 +0300

    Added post retrieve api view and added filters for blog posts list

--- backend/api/v1/posts/views.py
+++ backend/api/v1/posts/views.py
@@ -1,9 +1,12 @@
 import datetime
 
-from rest_framework.generics import ListAPIView
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 
+from api.v1.posts.filters import PrioritizedPostSearchFilter
 from api.v1.posts.pagination import PostPagination
-from api.v1.posts.serializers import PostListSerializer
+from api.v1.posts.serializers import PostListSerializer, PostDetailedSerializer
 from posts.models import Post
 
 
@@ -11,6 +14,10 @@ class PostListAPIView(ListAPIView):
     queryset = Post.objects.all()
     serializer_class = PostListSerializer
     pagination_class = PostPagination
+    filter_backends = (PrioritizedPostSearchFilter, filters.OrderingFilter, DjangoFilterBackend)
+    filterset_fields = ("user_id", "tags__id")
+    ordering_fields = ("title", "content", "published_at", "user_username")
+    ordering = ("-published_at",)
 
     def get_queryset(self):
         blog_id = self.kwargs["blog_id"]
@@ -19,3 +26,13 @@ class PostListAPIView(ListAPIView):
             .select_related("user")
             .prefetch_related("tags")
         )
+
+
+class PostRetrieveAPIView(RetrieveAPIView):
+    queryset = (
+        Post.objects.filter(published_at__lte=datetime.datetime.now(), status__exact="Published")
+        .select_related("user")
+        .prefetch_related("tags")
+    )
+    serializer_class = PostDetailedSerializer
+    lookup_field = "id"
--- backend/api/v1/urls.py
+++ backend/api/v1/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 
 from api.v1.blogs.views import BlogViewSet
 from api.v1.main_page.views import MainPagePostsAPIView
-from api.v1.posts.views import PostListAPIView
+from api.v1.posts.views import PostListAPIView, PostRetrieveAPIView
 from api.v1.users.views import RegisterView
 
 router = DefaultRouter()
@@ -15,5 +15,6 @@ urlpatterns = [
     path("", include(router.urls)),
     path("user/register/", RegisterView.as_view(), name="register"),
     path("main_page/posts/", MainPagePostsAPIView.as_view(), name="main_page_posts"),
-    path("blog_posts/<int:blog_id>", PostListAPIView.as_view(), name="published_posts"),
+    path("blog_posts/<int:blog_id>", PostListAPIView.as_view(), name="blog_posts"),
+    path("blog_post/<int:id>", PostRetrieveAPIView.as_view(), name="blog_post"),
 ]
