a5d2f0944d138872e94a907c9ba02a5fbcfe5975
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-15 16:02:33 +0300

    Tag name and slug is unique

--- backend/tags/models.py
+++ backend/tags/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Tag(models.Model):
     """Модель метки для постов."""
 
-    name = models.CharField(max_length=30, help_text="Название тега")
-    slug = models.SlugField(max_length=40, help_text="Слаг")
+    name = models.CharField(max_length=30, help_text="Название тега", unique=True)
+    slug = models.SlugField(max_length=40, help_text="Слаг", unique=True)
 
     def __str__(self):
         return self.name
