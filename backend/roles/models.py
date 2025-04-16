ef6f577bd8d6d56e6d6f74ff229200505d33ef5c
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-16 16:31:13 +0300

    Added missed max_length for name field in Role model

--- backend/roles/models.py
+++ backend/roles/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 
 class Role(models.Model):
-    name = models.CharField(help_text="Название роли")
+    name = models.CharField(help_text="Название роли", max_length=15)
