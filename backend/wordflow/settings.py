d5c9a517e6c572bde433a0ac33bf3bc7e8dd329b
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-16 08:01:54 +0300

    Change DATA_UPLOAD_MAX_MEMORY_SIZE to 5nb

--- backend/wordflow/settings.py
+++ backend/wordflow/settings.py
@@ -140,6 +140,8 @@ STATIC_URL = "static/"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = "/media"
 
+DATA_UPLOAD_MAX_MEMORY_SIZE = 5 * 1024 * 1024
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
