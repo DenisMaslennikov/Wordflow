5f1e408ecbb8329dbb5f632bc2295ae0eedcdc7c
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-16 12:00:09 +0300

    Change user by id endpoints to user/me endpoint.

--- backend/api/v1/users/views.py
+++ backend/api/v1/users/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
-from rest_framework import generics, viewsets, parsers, renderers
+from rest_framework import viewsets, parsers, renderers, mixins, decorators
+from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from .permissions import IsMeOrReadOnly
@@ -14,7 +15,7 @@ class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin, mixins.RetrieveModelMixin):
     """Вьюсет пользователя."""
 
     queryset = User.objects.all()
@@ -22,3 +23,23 @@ class UserViewSet(viewsets.ModelViewSet):
     renderer_classes = (renderers.JSONRenderer,)
     serializer_class = UserSerializer
     permission_classes = (IsMeOrReadOnly,)
+
+    @decorators.action(detail=False, methods=["put", "patch", "get"], permission_classes=[IsMeOrReadOnly])
+    def me(self, request):
+        """Эндпоинты для реализации редактирования профиля текущего пользователя."""
+
+        user = request.user
+
+        if request.method == "GET":
+            serializer = UserSerializer(user)
+            return Response(serializer.data)
+        elif request.method == "PATCH":
+            serializer = self.get_serializer(user, data=request.data, partial=True)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
+        elif request.method == "PUT":
+            serializer = self.get_serializer(user, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
