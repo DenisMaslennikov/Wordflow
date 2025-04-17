fdb54ba787b7e21997cd5a00799a4e41c74dd120
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-17 12:41:50 +0300

    Added Style constants

---
+++ frontend/src/utils/constants.ts
@@ -0,0 +1,8 @@
+// Design
+
+// COLORS
+export const BORDER_COLOR = "--color-grey-300";
+export const BACKGROUND_COLOR = "--color-grey-0";
+export const HOVER_COLOR = "--color-grey-100";
+export const BUTTONS_MAIN_COLOR = "--color-grey-500";
+export const TEXT_MAIN_COLOR = "--color-grey-800";
