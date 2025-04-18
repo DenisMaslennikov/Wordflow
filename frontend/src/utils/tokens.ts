8f6583ac514049a2d96d91b084a59f8852c39c09
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-18 15:54:40 +0300

    Added tokens store in localStorage

---
+++ frontend/src/utils/tokens.ts
@@ -0,0 +1,14 @@
+import { ACCESS_KEY, REFRESH_KEY } from "./constants.ts";
+
+export const tokenStorage = {
+  getAccess: () => localStorage.getItem(ACCESS_KEY),
+  getRefresh: () => localStorage.getItem(REFRESH_KEY),
+  set: (access: string, refresh: string) => {
+    localStorage.setItem(ACCESS_KEY, access);
+    localStorage.setItem(REFRESH_KEY, refresh);
+  },
+  clear: () => {
+    localStorage.removeItem(ACCESS_KEY);
+    localStorage.removeItem(REFRESH_KEY);
+  },
+};
