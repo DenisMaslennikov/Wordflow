5e1d375647d7b199001c9e5ca7f02a9e2e76e5ff
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-18 11:53:54 +0300

    Added a placeholder for the user

---
+++ frontend/src/hooks/useUser.ts
@@ -0,0 +1,12 @@
+function useUser() {
+  return {
+    id: 4,
+    username: "sd",
+    first_name: "",
+    last_name: "",
+    bio: null,
+    avatar: "http://127.0.0.1:8000/media/users/111111111111111111_cB9CyAV.png",
+  };
+}
+
+export default useUser;
