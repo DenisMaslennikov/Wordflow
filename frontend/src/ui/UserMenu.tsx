31c34407d4ea56b2242809ca8903e4827e4a4f77
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-18 15:21:37 +0300

    Added loading mini spinner

--- frontend/src/ui/UserMenu.tsx
+++ frontend/src/ui/UserMenu.tsx
@@ -1,9 +1,12 @@
 import useUser from "../hooks/useUser.ts";
 import AuthorizedUserMenu from "../features/authentication/AuthorizedUserMenu.tsx";
 import UnauthorizedUserMenu from "../features/authentication/UnauthorizedUserMenu.tsx";
+import SpinnerMini from "./SpinnerMini.tsx";
 
 function UserMenu() {
   const { isUserLoading, user } = useUser();
+
+  if (isUserLoading) return <SpinnerMini />;
   return user?.id ? <AuthorizedUserMenu /> : <UnauthorizedUserMenu />;
 }
 
