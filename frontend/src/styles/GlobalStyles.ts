498a955b03937e95b0aa12f8d476108194c0a571
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-18 08:33:31 +0300

    added some global styles

--- frontend/src/styles/GlobalStyles.ts
+++ frontend/src/styles/GlobalStyles.ts
@@ -18,24 +18,31 @@ const GlobalStyles = createGlobalStyle`
         }
 
         &.dark-mode {
-            --color-grey-0: #19171d;
-            --color-grey-50: #2c2c3d;
-            --color-grey-100: #414156;
-            --color-grey-200: #5c5c70;
-            --color-grey-300: #7e7e92;
-            --color-grey-400: #a9a9b7;
-            --color-grey-500: #d4d4db;
-            --color-grey-600: #e8e8ec;
-            --color-grey-700: #f3f3f5;
-            --color-grey-800: #f9f9fa;
-            --color-grey-900: #fff;
+          --color-grey-0: #19171d;
+          --color-grey-50: #2c2c3d;
+          --color-grey-100: #414156;
+          --color-grey-200: #5c5c70;
+          --color-grey-300: #7e7e92;
+          --color-grey-400: #a9a9b7;
+          --color-grey-500: #d4d4db;
+          --color-grey-600: #e8e8ec;
+          --color-grey-700: #f3f3f5;
+          --color-grey-800: #f9f9fa;
+          --color-grey-900: #fff;
         }
-
-        //--color-hover: #769eef;
+      
+        --color-hover: #769eef;
         --color-active: #769eef;
+      
+        --border-radius-tiny: 3px;
+        --border-radius-sm: 5px;
+        --border-radius-md: 7px;
+        --border-radius-lg: 9px;
     }
 
-    * {
+    *,
+    *::before,
+    *::after {
         box-sizing: border-box;
         padding: 0;
         margin: 0;
