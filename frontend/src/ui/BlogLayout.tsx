fea8ab834bd86122a7e4ebe3899c4b5c721f8640
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-18 10:27:55 +0300

    Added main header

--- frontend/src/ui/BlogLayout.tsx
+++ frontend/src/ui/BlogLayout.tsx
@@ -1,10 +1,22 @@
 import { Outlet } from "react-router-dom";
+import Header from "./Header.tsx";
+import Logo from "./Logo.tsx";
+import BlogMenu from "./BlogMenu.tsx";
 
 function BlogLayout() {
   return (
-    <div>
+    <>
+      <Header>
+        <Header.LeftHeaderBlock>
+          <Logo />
+          <BlogMenu />
+        </Header.LeftHeaderBlock>
+        <Header.RightHeaderBlock>
+          <button>Регистрация</button>
+        </Header.RightHeaderBlock>
+      </Header>
       <Outlet />
-    </div>
+    </>
   );
 }
 
---
+++ frontend/src/ui/Header.tsx
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+import { BORDER_COLOR } from "../utils/constants.ts";
+import React from "react";
+
+const StyledHeader = styled.header`
+  height: auto;
+  border-bottom: 1px solid var(${BORDER_COLOR});
+  width: 100vw;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  gap: 2rem;
+  padding: 0.5rem 1rem;
+`;
+
+const LeftHeaderBlock = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  flex-direction: row;
+  gap: 2rem;
+`;
+
+const RightHeaderBlock = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  flex-direction: row;
+`;
+
+function Header({ children }: { children: React.ReactNode }) {
+  return <StyledHeader>{children}</StyledHeader>;
+}
+
+Header.LeftHeaderBlock = LeftHeaderBlock;
+Header.RightHeaderBlock = RightHeaderBlock;
+
+export default Header;
