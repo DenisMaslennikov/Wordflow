c1677000bcc7b3bc1d13211defc28fa70b786efc
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-18 09:37:56 +0300

    Added logo text

--- frontend/src/ui/Logo.tsx
+++ frontend/src/ui/Logo.tsx
@@ -1,12 +1,33 @@
 import styled from "styled-components";
+import { TEXT_MAIN_COLOR } from "../utils/constants.ts";
 
 const Img = styled.img`
   height: 3rem;
   width: auto;
 `;
 
+const LogoContainer = styled.div`
+  display: flex;
+  gap: 0.5rem;
+  text-align: center;
+  align-items: center;
+`;
+
+const LogoText = styled.span`
+  color: var(${TEXT_MAIN_COLOR});
+  font-size: 2rem;
+  font-weight: 400;
+  font-family: "Permanent Marker", cursive;
+  font-style: normal;
+`;
+
 function Logo() {
-  return <Img src={"/logo.png"} alt={"Logo"}></Img>;
+  return (
+    <LogoContainer>
+      <Img src={"/logo.png"} alt={"Logo"}></Img>
+      <LogoText>Wordflow</LogoText>
+    </LogoContainer>
+  );
 }
 
 export default Logo;
