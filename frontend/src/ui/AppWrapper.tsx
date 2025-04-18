72cf7c5f01245a82287a2da5cdb60e349bccdd26
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-18 08:39:33 +0300

    Added bg color

--- frontend/src/ui/AppWrapper.tsx
+++ frontend/src/ui/AppWrapper.tsx
@@ -1,9 +1,11 @@
 import styled from "styled-components";
+import { BACKGROUND_COLOR } from "../utils/constants.ts";
 
 const AppWrapper = styled.div`
   display: flex;
   flex-direction: column;
   height: 100vh;
+  background-color: var(${BACKGROUND_COLOR});
 `;
 
 export default AppWrapper;
