cf2c7ec8d25b713505e2b2675b5086e052bd9159
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-21 14:45:24 +0300

    Add useAuth hook to component

--- frontend/src/ui/UserMenu.tsx
+++ frontend/src/ui/UserMenu.tsx
@@ -1,13 +1,15 @@
-import useUser from "../features/authentication/hooks/useUser.ts";
 import AuthorizedUserMenu from "../features/authentication/AuthorizedUserMenu.tsx";
 import UnauthorizedUserMenu from "../features/authentication/UnauthorizedUserMenu.tsx";
 import SpinnerMini from "./SpinnerMini.tsx";
+import useAuth from "../hooks/useAuth.ts";
+import useUser from "../features/authentication/hooks/useUser.ts";
 
 function UserMenu() {
-  const { isUserLoading, user } = useUser();
+  const { isAuthenticated } = useAuth();
+  const { isUserLoading } = useUser();
 
   if (isUserLoading) return <SpinnerMini />;
-  return user?.id ? <AuthorizedUserMenu /> : <UnauthorizedUserMenu />;
+  return isAuthenticated ? <AuthorizedUserMenu /> : <UnauthorizedUserMenu />;
 }
 
 export default UserMenu;
