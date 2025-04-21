5c0021723bb53b654b03f3e8c0e3f43e5489a374
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-21 14:41:42 +0300

    Move user stale time to constants

--- frontend/src/features/authentication/hooks/useUser.ts
+++ frontend/src/features/authentication/hooks/useUser.ts
@@ -1,11 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
+
 import { userService } from "../api/userService.ts";
+import { USER_PROFILE_STALE_TIME } from "../../../utils/constants.ts";
 
 function useUser() {
   const { data: user, isLoading: isUserLoading } = useQuery({
     queryFn: userService.getUserMe,
     queryKey: ["user"],
-    staleTime: 60_000,
+    staleTime: USER_PROFILE_STALE_TIME,
   });
   return { isUserLoading, user };
 }
--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -7,6 +7,9 @@ export const BUTTON_TEXT_COLOR = "--color-grey-500";
 export const TEXT_MAIN_COLOR = "--color-grey-800";
 export const HOVER_COLOR = "--color-grey-50";
 
-//TOKEN LOCAL STORAGE
+// TOKEN LOCAL STORAGE
 export const ACCESS_KEY = "accessToken";
 export const REFRESH_KEY = "refreshToken";
+
+// STALE TIME FOR REACT-QUERY QUERIES
+export const USER_PROFILE_STALE_TIME = 60 * 60 * 1000;
