8948f9291b8480d176ccc380f8a4fc7a8a22486f
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-21 14:33:51 +0300

    Update useAuth hook

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -3,7 +3,10 @@ import { useContext } from "react";
 import { AuthContext } from "../context/Auth/AuthContext.ts";
 
 function useAuth() {
-  return useContext(AuthContext);
+  const context = useContext(AuthContext);
+  if (!context)
+    throw new Error("useAuthuseAuthContext must be used within AuthProvider");
+  return context;
 }
 
 export default useAuth;
