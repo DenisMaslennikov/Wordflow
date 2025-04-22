0b3e4a64955df455b92cdcdf21fa8a1ad01ff7ba
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-22 13:58:38 +0300

    Change hover color

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -5,7 +5,7 @@ export const BORDER_COLOR = "--color-grey-300";
 export const BACKGROUND_COLOR = "--color-grey-0";
 export const BUTTON_TEXT_COLOR = "--color-grey-500";
 export const TEXT_MAIN_COLOR = "--color-grey-800";
-export const HOVER_COLOR = "--color-grey-50";
+export const HOVER_COLOR = "--color-grey-100";
 
 // TOKEN LOCAL STORAGE
 export const ACCESS_KEY = "accessToken";
