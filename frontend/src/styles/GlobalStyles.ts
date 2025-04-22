c0df843e052327d6440b0bd7d20c71af5c60f685
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-22 11:16:25 +0300

    Added color-red palette

--- frontend/src/styles/GlobalStyles.ts
+++ frontend/src/styles/GlobalStyles.ts
@@ -5,19 +5,23 @@ const GlobalStyles = createGlobalStyle`
   
     :root {
         &, &.light-mode {
-            --color-grey-0: #fff;
-            --color-grey-50: #f9f9fa;
-            --color-grey-100: #f3f3f5;
-            --color-grey-200: #e8e8ec;
-            --color-grey-300: #d4d4db;
-            --color-grey-400: #a9a9b7;
-            --color-grey-500: #7e7e92;
-            --color-grey-600: #5c5c70;
-            --color-grey-700: #414156;
-            --color-grey-800: #2c2c3d;
-            --color-grey-900: #19171d;
+          --color-grey-0: #fff;
+          --color-grey-50: #f9f9fa;
+          --color-grey-100: #f3f3f5;
+          --color-grey-200: #e8e8ec;
+          --color-grey-300: #d4d4db;
+          --color-grey-400: #a9a9b7;
+          --color-grey-500: #7e7e92;
+          --color-grey-600: #5c5c70;
+          --color-grey-700: #414156;
+          --color-grey-800: #2c2c3d;
+          --color-grey-900: #19171d;
 
-            --backdrop-color: rgba(255, 255, 255, 0.1);
+          --color-red-100: #fee2e2;
+          --color-red-700: #b91c1c;
+          --color-red-800: #991b1b;
+
+          --backdrop-color: rgba(255, 255, 255, 0.1);
         }
 
         &.dark-mode {
@@ -32,6 +36,10 @@ const GlobalStyles = createGlobalStyle`
           --color-grey-700: #f3f3f5;
           --color-grey-800: #f9f9fa;
           --color-grey-900: #fff;
+
+          --color-red-100: #fee2e2;
+          --color-red-700: #b91c1c;
+          --color-red-800: #991b1b;
 
           --backdrop-color: rgba(0, 0, 0, 0.3);
         }
