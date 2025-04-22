b6d9caca1350d301097479cdf93ae073257d2509
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-22 12:32:05 +0300

    Added TextArea

---
+++ frontend/src/ui/TextArea.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const TextArea = styled.textarea`
+  border: 1px solid var(--color-grey-300);
+  background-color: var(--color-grey-0);
+  border-radius: var(--border-radius-sm);
+  padding: 0.5rem;
+`;
+
+export default TextArea;
