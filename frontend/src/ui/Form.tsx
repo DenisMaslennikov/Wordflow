3ac7891936fdfcec905a04f1b29078bebf4205a8
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-22 12:16:17 +0300

    Removed border

--- frontend/src/ui/Form.tsx
+++ frontend/src/ui/Form.tsx
@@ -1,9 +1,5 @@
 import styled from "styled-components";
-import { BORDER_COLOR } from "../utils/constants.ts";
 
-const Form = styled.form`
-  border-radius: var(--border-radius-md);
-  border: 1px solid var(${BORDER_COLOR});
-`;
+const Form = styled.form``;
 
 export default Form;
