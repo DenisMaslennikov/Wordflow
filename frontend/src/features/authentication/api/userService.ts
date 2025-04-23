f22ec07f89d44c1d5cce11c288482f9f4c153c78
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-23 14:50:53 +0300

    Added update user me function. Also rework create user to work with FileList

--- frontend/src/features/authentication/api/userService.ts
+++ frontend/src/features/authentication/api/userService.ts
@@ -10,7 +10,25 @@ const userService = {
   },
 
   createUser: async (data: UserForm) => {
-    const response = await apiClient.post<UserProfile>("user/", data, {
+    const body =
+      data.avatar instanceof FileList
+        ? { ...data, avatar: data.avatar[0] }
+        : data;
+    const response = await apiClient.post<UserProfile>("user/", body, {
+      headers: { "Content-Type": "multipart/form-data" },
+    });
+    return response.data;
+  },
+
+  updateUserMe: async (data: UserForm) => {
+    const body =
+      typeof data.avatar === "string"
+        ? // eslint-disable-next-line @typescript-eslint/no-unused-vars
+          (({ avatar, ...rest }) => rest)(data)
+        : data.avatar instanceof FileList
+          ? { ...data, avatar: data.avatar[0] }
+          : data;
+    const response = await apiClient.patch<UserProfile>("user/me/", body, {
       headers: { "Content-Type": "multipart/form-data" },
     });
     return response.data;
