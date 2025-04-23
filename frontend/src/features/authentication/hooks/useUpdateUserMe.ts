e2de7eee22eb35f0eddaf5377ff50a0369a1a4ce
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-23 14:57:14 +0300

    Added useUpdateUserMe hook

---
+++ frontend/src/features/authentication/hooks/useUpdateUserMe.ts
@@ -0,0 +1,19 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+
+import { userService } from "../api/userService.ts";
+
+function useUpdateUserMe() {
+  const queryClient = useQueryClient();
+
+  const { mutate: updateUserMe, isPending: isUserUpdating } = useMutation({
+    mutationFn: userService.updateUserMe,
+    onSuccess: (data) => {
+      queryClient.setQueriesData({ queryKey: ["user"] }, data);
+      toast.success("Данные пользователя обновлены");
+    },
+  });
+  return { updateUserMe, isUserUpdating };
+}
+
+export default useUpdateUserMe;
