0aaf0946a3150fe040f786017a91f5fcacb9c77c
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-24 15:53:08 +0300

    Removed main page view

--- backend/api/v1/urls.py
+++ backend/api/v1/urls.py
@@ -4,7 +4,6 @@ from rest_framework.routers import DefaultRouter
 from api.v1.blogs.views import BlogViewSet
 from api.v1.comments.views import CommentViewSet
 from api.v1.images.views import ImageViewSet
-from api.v1.main_page.views import MainPagePostsListAPIView
 from api.v1.posts.views import PostForAuthorViewSet, PostListAPIView, PostRetrieveAPIView
 from api.v1.roles.views import RolesListAPIView
 from api.v1.tags.views import TagsViewSet
@@ -29,7 +28,6 @@ urlpatterns = [
     path("post_editor/<int:blog_id>/", include(blog_router.urls)),
     path("blog_images/<slug:blog_slug>/", include(images_router.urls)),
     path("post_comments/<int:post_id>/", include(comments_router.urls)),
-    # path("main_page/posts/", MainPagePostsListAPIView.as_view(), name="main_page_posts"),
     path("posts/", PostListAPIView.as_view(), name="posts"),
     path("blog_post/<int:id>/", PostRetrieveAPIView.as_view(), name="blog_post"),
     path("roles/", RolesListAPIView.as_view(), name="roles"),
