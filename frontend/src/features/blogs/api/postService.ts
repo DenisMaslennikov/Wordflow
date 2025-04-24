702dbd895485a73c72838ee08e0c9e0d15828049
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-24 17:34:20 +0300

    Added api request params

--- frontend/src/features/blogs/api/postService.ts
+++ frontend/src/features/blogs/api/postService.ts
@@ -2,9 +2,16 @@ import apiClient from "../../../service/apiClient.ts";
 import { PostApi } from "../types/Post.ts";
 import { PaginatedResults } from "../../../types/PaginatedResults.ts";
 
+interface GetPostsParams {
+  limit: number;
+  offset: number;
+}
+
 export const postService = {
-  getPosts: async () => {
-    const response = await apiClient.get<PaginatedResults<PostApi>>("posts/");
+  getPosts: async ({ limit, offset }: GetPostsParams) => {
+    const response = await apiClient.get<PaginatedResults<PostApi>>("posts/", {
+      params: { limit, offset },
+    });
     return response.data;
   },
 };
