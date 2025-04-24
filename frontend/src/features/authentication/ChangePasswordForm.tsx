4519e6c35fea124fc2a0a99672f1416ca1dc0d0c
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-24 11:39:55 +0300

    Added ChangePasswordForm component

---
+++ frontend/src/features/authentication/ChangePasswordForm.tsx
@@ -0,0 +1,79 @@
+import { useForm } from "react-hook-form";
+import type { ChangePasswordForm } from "./types/User.ts";
+import Form from "../../ui/Form.tsx";
+import FormRowVertical from "../../ui/FormRowVertical.tsx";
+import Input from "../../ui/Input.tsx";
+import {
+  PASSWORD_MIN_LENGTH,
+  WIDTH_LOGIN_MODAL,
+} from "../../utils/constants.ts";
+import usePasswordChange from "./hooks/usePasswordChange.ts";
+import FormRowHorizontal from "../../ui/FormRowHorizontal.tsx";
+import Button from "../../ui/Button.tsx";
+
+function ChangePasswordForm({ onCloseModal }: { onCloseModal?: () => void }) {
+  const { register, formState, handleSubmit } = useForm<ChangePasswordForm>();
+  const { changePassword, isPasswordChanging } = usePasswordChange();
+
+  const { errors } = formState;
+
+  function onSubmit(form: ChangePasswordForm) {
+    changePassword(form, { onSuccess: () => onCloseModal?.() });
+  }
+
+  return (
+    <Form $width={WIDTH_LOGIN_MODAL} onSubmit={handleSubmit(onSubmit)}>
+      <Input
+        type="text"
+        name="username"
+        autoComplete="username"
+        style={{ display: "none" }}
+        tabIndex={-1}
+      />
+      <FormRowVertical label={"Новый пароль"} error={errors?.password?.message}>
+        <Input
+          autoComplete={"new-password"}
+          id={"password"}
+          disabled={isPasswordChanging}
+          placeholder={"Новый пароль"}
+          type={"password"}
+          {...register("password", {
+            required: "Не указан пароль",
+            minLength: {
+              value: PASSWORD_MIN_LENGTH,
+              message: `Минимальная длинна пароля ${PASSWORD_MIN_LENGTH} символов`,
+            },
+          })}
+        />
+      </FormRowVertical>
+      <FormRowVertical
+        label={"Повтор пароля"}
+        error={errors?.repeatPassword?.message}
+      >
+        <Input
+          id={"repeatPassword"}
+          autoComplete={"new-password"}
+          disabled={isPasswordChanging}
+          placeholder={"Повтор пароля"}
+          type={"password"}
+          {...register("repeatPassword", {
+            required: "Введите пароль повторно",
+            validate: (value, formValues) =>
+              value === formValues.password || "Пароли не совпадают",
+          })}
+        />
+      </FormRowVertical>
+      <FormRowHorizontal>
+        <Button
+          $size={"mediumFullWidth"}
+          $style={"regular"}
+          disabled={isPasswordChanging}
+        >
+          Изменить
+        </Button>
+      </FormRowHorizontal>
+    </Form>
+  );
+}
+
+export default ChangePasswordForm;
