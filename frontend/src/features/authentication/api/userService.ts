1db8ffd9012e19656d9594099f06ae5744944777
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-24 11:20:23 +0300

    Rework updateUserMe add ability change password

--- frontend/src/features/authentication/api/userService.ts
+++ frontend/src/features/authentication/api/userService.ts
@@ -1,10 +1,12 @@
 import apiClient from "../../../service/apiClient.ts";
 
-import type {
+import {
   LoginForm,
   UserSignupForm,
   UserProfile,
   UserUpdateForm,
+  ChangePasswordForm,
+  isUserUpdateForm,
 } from "../types/User.ts";
 import type { TokensPair } from "../types/Tokens.ts";
 import { isAxiosError } from "axios";
@@ -36,14 +38,17 @@ const userService = {
     return response.data;
   },
 
-  updateUserMe: async (data: UserUpdateForm) => {
-    const body =
-      typeof data.avatar === "string"
-        ? // eslint-disable-next-line @typescript-eslint/no-unused-vars
-          (({ avatar, ...rest }) => rest)(data)
-        : data.avatar instanceof FileList
-          ? { ...data, avatar: data.avatar[0] }
-          : data;
+  updateUserMe: async (data: UserUpdateForm | ChangePasswordForm) => {
+    let body = data;
+    if (isUserUpdateForm(body)) {
+      body =
+        typeof body.avatar === "string"
+          ? // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            (({ avatar, ...rest }) => rest)(body)
+          : body.avatar instanceof FileList
+            ? { ...body, avatar: body.avatar[0] }
+            : body;
+    }
     const response = await apiClient.patch<UserProfile>("user/me/", body, {
       headers: { "Content-Type": "multipart/form-data" },
     });
