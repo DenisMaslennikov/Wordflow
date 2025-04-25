05f230d06c9310e6460771f83137a7aa5e347fb7
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-25 14:05:32 +0300

    Added hyphens

--- frontend/src/ui/Heading.tsx
+++ frontend/src/ui/Heading.tsx
@@ -6,18 +6,24 @@ const Heading = styled.h1<{ as: string }>`
     css`
       font-size: 3rem;
       font-weight: 600;
+      hyphens: auto;
+      word-break: break-word;
     `}
   ${(props) =>
     props.as === "h2" &&
     css`
       font-size: 2rem;
       font-weight: 600;
+      hyphens: auto;
+      word-break: break-word;
     `}  
   ${(props) =>
     props.as === "h3" &&
     css`
       font-size: 2rem;
       font-weight: 500;
+      hyphens: auto;
+      word-break: break-word;
     `}  
 
   line-height: 1.4
