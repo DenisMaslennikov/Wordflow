2ad4ed13cafecfc00e52e8b07656977fc4e0995a
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-25 14:54:21 +0300

    Added styles

--- frontend/src/features/blogs/PostCard.tsx
+++ frontend/src/features/blogs/PostCard.tsx
@@ -1,21 +1,47 @@
 import type { Post } from "./types/Post.ts";
 import Heading from "../../ui/Heading.tsx";
 import styled from "styled-components";
-import { BORDER_COLOR } from "../../utils/constants.ts";
+import {
+  BORDER_COLOR,
+  MAX_POST_PREVIEW_HEIGHT,
+  MAX_POST_PREVIEW_WIDTH,
+  MAX_WIDTH_POST_IN_LIST,
+} from "../../utils/constants.ts";
+import DOMPurify from "dompurify";
 
 const StyledPostCard = styled.div`
-  display: flex;
-  flex-direction: column;
   margin: 1rem auto;
   border: var(${BORDER_COLOR}) 1px solid;
-  max-width: 1024px;
+  max-width: ${MAX_WIDTH_POST_IN_LIST};
   width: 100%;
+  padding: 1rem 1rem;
+
+  & img {
+    max-width: ${MAX_POST_PREVIEW_WIDTH};
+    max-height: ${MAX_POST_PREVIEW_HEIGHT};
+  }
+`;
+
+const StyledTextBlock = styled.div`
+  hyphens: auto;
+  word-break: break-word;
+`;
+
+const Preview = styled.img`
+  float: left;
+  max-width: ${MAX_POST_PREVIEW_WIDTH};
+  max-height: ${MAX_POST_PREVIEW_HEIGHT};
+  margin-right: 1rem;
 `;
 
 function PostCard({ post }: { post: Post }) {
   return (
     <StyledPostCard>
       <Heading as={"h3"}>{post.title}</Heading>
+      {post.preview ? <Preview src={`${post.preview?.image}`} /> : null}
+      <StyledTextBlock
+        dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(post.content) }}
+      />
     </StyledPostCard>
   );
 }
