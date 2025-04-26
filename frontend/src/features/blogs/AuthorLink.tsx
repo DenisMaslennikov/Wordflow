a2fef2a9073e7a4b39a3161ee3da48627806c4cd
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-26 07:29:13 +0300

    Removed avatar

--- frontend/src/features/blogs/AuthorLink.tsx
+++ frontend/src/features/blogs/AuthorLink.tsx
@@ -1,6 +1,5 @@
 import { UserShort } from "../authentication/types/User.ts";
 import StyledLink from "../../ui/StyledLink.tsx";
-import Avatar from "../../ui/Avatar.tsx";
 import styled from "styled-components";
 
 const UserContainer = styled.div`
@@ -17,10 +16,7 @@ interface AuthorLinkProps {
 function AuthorLink({ author }: AuthorLinkProps) {
   return (
     <StyledLink to={`/user/${author.username}`}>
-      <UserContainer>
-        {author.username}
-        {author.avatar && <Avatar $size={"2rem"} src={author.avatar} />}
-      </UserContainer>
+      <UserContainer>{author.username}</UserContainer>
     </StyledLink>
   );
 }
