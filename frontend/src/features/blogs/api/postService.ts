29f3e1e952735f4116a8e11d0a387308dd0d2b67
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-26 10:58:13 +0300

    Added api endpoint for get single post by id

--- frontend/src/features/blogs/api/postService.ts
+++ frontend/src/features/blogs/api/postService.ts
@@ -1,5 +1,5 @@
 import apiClient from "../../../service/apiClient.ts";
-import { PostApi } from "../types/Post.ts";
+import { PostApi, PostDetailedApi } from "../types/Post.ts";
 import { PaginatedResults } from "../../../types/PaginatedResults.ts";
 
 interface GetPostsParams {
@@ -12,6 +12,12 @@ export const postService = {
     const response = await apiClient.get<PaginatedResults<PostApi>>("posts/", {
       params: { limit, offset },
     });
+    return response.data;
+  },
+  getPost: async (postId: number) => {
+    const response = await apiClient.get<PostDetailedApi>(
+      `blog_post/${postId}`,
+    );
     return response.data;
   },
 };
