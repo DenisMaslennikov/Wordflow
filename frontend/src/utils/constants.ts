0684441b2e89ed2a44897bb029886187e9417249
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-26 07:14:12 +0300

    Change Links Color

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -7,7 +7,7 @@ export const BACKGROUND_COLOR = "--color-grey-0";
 export const BUTTON_TEXT_COLOR = "--color-grey-500";
 export const TEXT_MAIN_COLOR = "--color-grey-800";
 export const HOVER_COLOR = "--color-grey-100";
-export const LINK_COLOR = "--color-brand-600";
+export const LINK_COLOR = "--color-brand-500";
 
 // Время на которое показывается вспывающее сообщение со статусом
 export const SUCCESS_TOAST_DURATION = 3000;
