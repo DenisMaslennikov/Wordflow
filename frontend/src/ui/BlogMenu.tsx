7ca8f5c8a660eb8cfc9c817f3b6c21b2e5f05dc2
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-27 09:13:54 +0300

    Move MenuLink, MenuLinkList and Nav to BlogMenu

--- frontend/src/ui/BlogMenu.tsx
+++ frontend/src/ui/BlogMenu.tsx
@@ -1,8 +1,55 @@
 import { HiHome, HiLink } from "react-icons/hi2";
-import { useParams } from "react-router-dom";
-import MenuLink from "./MenuLink.tsx";
-import Nav from "./Nav.tsx";
-import MenuLinkList from "./MenuLinkList.tsx";
+import { NavLink, useParams } from "react-router-dom";
+import styled from "styled-components";
+import { HOVER_COLOR } from "../utils/constants.ts";
+
+const Nav = styled.nav`
+  display: flex;
+  align-items: center;
+`;
+
+const MenuLinkList = styled.ul`
+  display: flex;
+  flex-direction: row;
+  gap: 0.5rem;
+  list-style: none;
+`;
+
+const MenuLink = styled(NavLink)`
+  &:link,
+  &:visited {
+    display: flex;
+    align-items: center;
+
+    gap: 0.3rem;
+
+    color: var(--color-grey-700);
+    font-size: 1.6rem;
+    font-weight: 500;
+    padding: 0.5rem;
+    transition: all 0.3s;
+  }
+
+  &:hover,
+  &:active,
+  &.active:link,
+  &.active:visited {
+    color: var(--color-active);
+    background-color: var(${HOVER_COLOR});
+    border-radius: var(--border-radius-sm);
+  }
+
+  & svg {
+    color: var(--color-grey-900);
+  }
+
+  &:hover svg,
+  &:active svg,
+  &.active:link svg,
+  &.active:visited svg {
+    color: var(--color-active);
+  }
+`;
 
 function BlogMenu() {
   const { blogSlug } = useParams();
--- frontend/src/ui/Nav.tsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-const Nav = styled.nav`
-  display: flex;
-  align-items: center;
-`;
-
-export default Nav;
