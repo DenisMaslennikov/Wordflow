eb1ed4f089425993f8ef76665e2b3e6d0680362d
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-27 11:26:08 +0300

    Added cache invalidation for current user and myBlogs while user login

--- frontend/src/features/authentication/hooks/useLogin.ts
+++ frontend/src/features/authentication/hooks/useLogin.ts
@@ -2,7 +2,10 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { userService } from "../api/userService.ts";
 import toast from "react-hot-toast";
 import { TokensPair } from "../types/Tokens.ts";
-import { USER_QUERY_KEY } from "../../../utils/constants.ts";
+import {
+  MY_BLOGS_QUERY_KEY,
+  USER_QUERY_KEY,
+} from "../../../utils/constants.ts";
 
 function useLogin({ setTokens }: { setTokens: (tokens: TokensPair) => void }) {
   const queryClient = useQueryClient();
@@ -10,7 +13,12 @@ function useLogin({ setTokens }: { setTokens: (tokens: TokensPair) => void }) {
     mutationFn: userService.login,
     onSuccess: (data) => {
       setTokens(data);
-      queryClient.invalidateQueries({ queryKey: [USER_QUERY_KEY] });
+      queryClient.invalidateQueries({
+        predicate: (query) =>
+          query.queryKey[0] === USER_QUERY_KEY ||
+          query.queryKey[0] === MY_BLOGS_QUERY_KEY,
+      });
+
       toast.success("Вы вошли в систему");
     },
     onError: () => {
